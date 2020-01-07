37d1a1bc46e738b49758b76b482a384f89af6c03
cindyroussel <58423453+cindyroussel@users.noreply.github.com>
2020-01-07 14:25:16 +0100

    update

--- src/js/map.js
+++ src/js/map.js
@@ -5,14 +5,23 @@ function initMap(listener) {
         lng: 3.058317
     };
 
-    let content = "<h5>Blue Home</h5> <p>Rue de la Clef</p><p>59800 Lille</p>";
+    let appt1 ={
+        lat : 50.627959,
+        lng : 3.059784
+    };
+
+    let content = '<p><img src="../src/img/nom.png" alt="logo" id="logo1" </p> <p>Rue de la Clef</p><p>59800 Lille</p>';
+    let content1 = "<h5>Réf. 123 Appt1</h5> <p>10 rue des Pyramides</p><p>59800 Lille</p>";
 
     let affichePlace = document.querySelector("#maps");
 
     let map = new google.maps.Map( affichePlace, {
-        zoom: 13,
+        zoom: 16,
         center: lille
+
+
     });
+
 
 
     let marker = new google.maps.Marker({
@@ -20,10 +29,26 @@ function initMap(listener) {
         map: map
     });
 
+    let marker1 = new google.maps.Marker({
+        position: appt1,
+        map: map
+    });
+
+
+
     let infos = new google.maps.InfoWindow({
         content: content,
-        position: lille
+        position: lille,
+
     });
+
+    let infos2 = new google.maps.InfoWindow({
+
+        content: content1,
+        position: appt1
+    });
+
+
 
     /* marker.addListener("click", function () {
          infos.open(map);
@@ -32,6 +57,10 @@ function initMap(listener) {
     marker.addListener("click", () => {
         infos.open(map);
     });
+    marker1.addListener("click", () => {
+        infos2.open(map);
+    });
+
 
 }
 
